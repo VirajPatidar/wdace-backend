401786b8ea00da3b09770d1f4be27b0cc38b4f22
Viraj Patidar <vrj381@gmail.com>
2022-09-21 20:29:28 +0530

    basic API

--- wdace/settings.py
+++ wdace/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%dmi*33j7b9&j6ff*y@6cu4yd3616j7nv0#o_6zg2v1xa=o34k'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wdace_api',
     'corsheaders',
 
     'rest_framework',
@@ -126,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ wdace_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WdaceApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wdace_api'
