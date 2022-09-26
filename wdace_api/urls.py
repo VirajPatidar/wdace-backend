e7b421eb87dc5176123e581e61c9a6419304d60f
Viraj Patidar <vrj381@gmail.com>
2022-09-26 19:20:52 +0530

    classify/analyse API (requires text processing)

--- wdace_api/urls.py
+++ wdace_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TestView
+from .views import TestView, ClassifyAnalyseView
 
 urlpatterns = [
 
+    path('classify', ClassifyAnalyseView.as_view(), name="classify"),
     path('test', TestView.as_view(), name="test"),
 
 ]
--- wdace_api/views.py
+++ wdace_api/views.py
@@ -3,7 +3,62 @@ from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
 
+import iso639
+from langdetect import detect
+
+
+from .utils.translate import detect_and_translate
+from .utils.extractive_summariser import extractive_summariser
+from .utils.keyword_extractor import keyword_extractor
+from .utils.scrape_url import getTextFromURL
+
 # Create your views here.
+
+
+class ClassifyAnalyseView(generics.GenericAPIView):
+
+    def post(self, request):
+
+        url = request.data.get('url')
+        text = getTextFromURL(url)
+
+        original_lang = iso639.to_name(detect(text))
+        translated_text = detect_and_translate(text, target_lang='en')
+        
+        keywords = []
+
+        if(translated_text == ""):
+            extractive_summary = extractive_summariser(text)
+            keywords = keyword_extractor(text)
+            translated_text_len = 0
+        else:
+            extractive_summary = extractive_summariser(translated_text)
+            keywords = keyword_extractor(translated_text)
+            translated_text_len = len(translated_text.split())
+
+
+        text_len = len(text.split())
+        extractive_summary_len = len(extractive_summary.split())
+
+
+        return Response(
+                        {
+                            "len": {
+                                "text_len": text_len,
+                                "translated_text_len": translated_text_len,
+                                "extractive_summary_len": extractive_summary_len,
+                            },
+                            "text": {
+                                "text": text,
+                                "translated_text": translated_text,
+                                "extractive_summary": extractive_summary,
+                            },
+                            "keywords": keywords,
+                            "original_lang": original_lang
+                        }, status=status.HTTP_201_CREATED)
+
+
+
 
 class TestView(generics.GenericAPIView):
 
