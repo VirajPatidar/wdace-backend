9008eaa00f14a67e3baba93797533e58ce25c5ac
Viraj Patidar <vrj381@gmail.com>
2022-10-01 14:22:10 +0530

    preprocessing done for translation

--- wdace_api/utils/translate.py
+++ wdace_api/utils/translate.py
@@ -1,16 +1,28 @@
+import re
 from langdetect import detect
 from googletrans import Translator
 
-#simple function to detect and translate text 
+
+def preProcessForTranslation(text):
+    # removing english words
+    txt_list = list(text.split(" "))
+    txt = list(filter(lambda ele: re.search("[a-zA-Z\s]+", ele) is None, txt_list))
+    text = ' '.join([str(elem) for elem in txt])
+
+    return str(text.strip())
+
+
+
 def detect_and_translate(text, target_lang):
     
     result_lang = detect(text)
     # print(result_lang)
     
     if result_lang == target_lang:
-        return "" 
+        return text 
     
     else:
         translator= Translator()
+        text = preProcessForTranslation(text)
         translation = translator.translate(text, src=result_lang, dest=target_lang)
         return translation.text
