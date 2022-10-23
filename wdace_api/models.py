e12acf053503c54777c40ff95aa42e57f16c5a3f
atharvadpatil <atharva.dp@somaiya.edu>
2022-10-24 00:10:32 +0530

    created Neo4j node

--- wdace_api/models.py
+++ wdace_api/models.py
@@ -2,20 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-from neomodel import StructuredNode, StringProperty, IntegerProperty,UniqueIdProperty, RelationshipTo
-
-# Create your models here.
+from neomodel import StructuredNode, StringProperty, IntegerProperty,UniqueIdProperty, RelationshipTo, FloatProperty, ArrayProperty
 
 
-class City(StructuredNode):
-    code = StringProperty(unique_index=True, required=True)
-    name = StringProperty(index=True, default="city")
 
-class Person(StructuredNode):
+class Topic(StructuredNode):
     uid = UniqueIdProperty()
-    name = StringProperty(unique_index=True)
-    age = IntegerProperty(index=True, default=0)
+    name = StringProperty(unique_index=True, required=True)
+    level = IntegerProperty(required=True)
+    weight = FloatProperty(default=0.5)
+    urls = ArrayProperty(StringProperty())
 
-    # Relations :
-    city = RelationshipTo(City, 'LIVES_IN')
-    friends = RelationshipTo('Person','FRIEND')
+    #Relations
+    hasTopic = RelationshipTo('Topic', 'HAS_TOPIC')
