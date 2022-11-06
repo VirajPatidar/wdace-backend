e44d138aea3a621fd34a086f4ad6256ad776fec2
Viraj Patidar <vrj381@gmail.com>
2022-11-06 13:49:22 +0530

    text preprocessing for topic extraction

--- wdace_api/utils/getDomainTopics.py
+++ wdace_api/utils/getDomainTopics.py
@@ -1,10 +1,38 @@
 import yake
+import string
+import nltk
+# nltk.download('stopwords')
+# nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import spacy
+
 
 
 def getDomainTopics(text):
     
-    # optimise parameters to get best results
+    # PREPROCESSING
+    # 1. Remove punctuation
+    # 2. Remove digits
+    # 3. Convert to lowercase
+    # 4. Remove stop words
 
+
+    def remove_punctuation(text):
+        punctuationfree="".join([i for i in text if i not in string.punctuation])
+        return punctuationfree
+
+    text=remove_punctuation(text)
+    text = text.lower()
+    text = ''.join([i for i in text if not i.isdigit()])
+    text=" ".join(text.split())
+
+
+
+
+
+    # YAKE
+    # optimise parameters to get best results
     language = "en"
     max_ngram_size = 2
     deduplication_thresold = 0.9
@@ -22,8 +50,13 @@ def getDomainTopics(text):
 
     keywords = kw_extractor.extract_keywords(text)
 
-    # for kw, v in keywords:
-    #     print("Keyphrase: ",kw, ": score", v)
+
+    for kw, v in keywords:
+        if(100-v > 99.999):
+          words = kw.split()
+          keyw = " ".join(sorted(set(words), key=words.index))
+          print("Keyphrase: ",keyw, ": score", 100-v)
+
 
     domain = keywords[0]
     topics = list(map(list, zip(*keywords[1:])))
