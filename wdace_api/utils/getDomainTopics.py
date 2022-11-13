9984a51d2f8fb22803786912b8f641d9ad203742
atharvadpatil <atharva.dp@somaiya.edu>
2022-11-13 13:34:01 +0530

    remove similar topics function added

--- wdace_api/utils/getDomainTopics.py
+++ wdace_api/utils/getDomainTopics.py
@@ -33,6 +33,53 @@ def pre_process(titles):
 
     return preprocessed_title_docs
 
+def similarity_filter(titles):
+    """
+    Recursively check if titles pass a similarity filter.
+    :param titles: list of strings, contains titles.
+    If the function finds titles that fail the similarity test, the above param will be the function output.
+    :return: this method upon itself unless there are no similar titles; in that case the feed that was passed
+    in is returned.
+    """
+
+    # Preprocess titles
+    preprocessed_title_docs = pre_process(titles)
+    # print(preprocessed_title_docs)
+    # Remove similar titles
+    all_summary_pairs = list(combinations(preprocessed_title_docs, 2))
+    similar_titles = []
+    for pair in all_summary_pairs:
+        title1 = nlp(pair[0])
+        title2 = nlp(pair[1])
+        similarity = title1.similarity(title2)
+        if similarity > 0.8:
+            similar_titles.append(pair)
+
+    titles_to_remove = []
+    for a_title in similar_titles:
+        # Get the index of the first title in the pair
+        index_for_removal = preprocessed_title_docs.index(a_title[1])
+        titles_to_remove.append(index_for_removal)
+
+    # Get indices of similar titles and remove them
+    similar_title_counts = set(titles_to_remove)
+    similar_titles = [
+        x[1] for x in enumerate(titles) if x[0] in similar_title_counts
+    ]
+
+    # Exit the recursion if there are no longer any similar titles
+    if len(similar_title_counts) == 0:
+        return titles
+
+    # Continue the recursion if there are still titles to remove
+    else:
+        # Remove similar titles from the next input
+        for title in similar_titles:
+            idx = titles.index(title)
+            titles.pop(idx)
+            
+        return similarity_filter(titles)
+
 
 def getDomainTopics(text):
     
