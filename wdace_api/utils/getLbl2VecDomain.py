e3739609e144ee12579c290a7704f87392771023
Viraj Patidar <vrj381@gmail.com>
2022-11-14 15:48:30 +0530

    getLbL2VecDomain() temp substitute

---
+++ wdace_api/utils/getLbl2VecDomain.py
@@ -0,0 +1,15 @@
+import random
+
+# LBL2VEC related tasks will come here
+ 
+def getDomain():
+
+    LbL2VecDomain = [
+        'E-Commerce',
+        'News',
+        'Blogs and Educational',
+        'Business website/Gaming or Others',
+        'Social Media and Entertainment'
+    ]
+
+    return random.choice(LbL2VecDomain)
